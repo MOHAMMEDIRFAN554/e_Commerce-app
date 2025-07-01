4726c0637c6a8c5b153d8d73d662180c8f5fbfc1
Mohammed Irfan <mohammedirfu554@gmail.com>
2025-07-01 20:04:33 +0530

    Updated cart screen and plugin registrants

--- lib/views/cart_screen/cart_screen.dart
+++ lib/views/cart_screen/cart_screen.dart
@@ -1,10 +1,19 @@
+import 'package:e_app/consts/consts.dart';
 import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.greenAccent);
+    return Container(
+      color: Colors.white,
+      child:
+          "Cart is Empty!...".text
+              .fontFamily(semibold)
+              .color(darkFontGrey)
+              .makeCentered(),
+    );
   }
 }
